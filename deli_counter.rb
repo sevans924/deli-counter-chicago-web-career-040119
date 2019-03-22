1596a1f94c2caf6a3280a3fe84f42246682e8cda
Sarah Elzinga <sevans924@gmail.com>
2019-03-22 17:56:51 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,12 +3,12 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length = 0
     return "The line is currently empty."
-    else 
+    else
 katz_line = []
 katz_deli.each_with_index { |name, index| katz_line.push("#{index}. #{name}")}
 end
-      return "The line is currently: #{katz_line.join(', ')}" 
+      return "The line is currently: #{katz_line.join(', ')}"
     end
-  
-    
+
+
   # Write your code here.
