4b9293494ac6eedc07c9d55904e6af71ed760193
Sarah Elzinga <sevans924@gmail.com>
2019-03-22 18:00:19 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,8 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.length == 0 
-    return "The line is currently empty."
+  if katz_deli.length == 0
+    puts "The line is currently empty."
     else
 katz_line = []
 katz_deli.each_with_index { |name, index| katz_line.push("#{index}. #{name}")}
